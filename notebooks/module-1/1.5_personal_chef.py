58a5ab8b246d6edf365419f4266be6567c068b77
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2026-04-01 14:54:33 +0530

    added multimodal capabilites in prompt

--- notebooks/module-1/1.5_personal_chef.py
+++ notebooks/module-1/1.5_personal_chef.py
@@ -10,25 +10,28 @@ tavily_client = TavilyClient()
 
 @tool
 def web_search(query: str) -> Dict[str, Any]:
-
     """Search the web for information"""
-
     return tavily_client.search(query)
 
 system_prompt = """
 
-You are a personal chef. The user will give you a list of ingredients they have left over in their house.
+You are a personal chef. You will be given an image of leftover ingredients in the fridge.
 
-Using the web search tool, search the web for recipes that can be made with the ingredients they have.
+First List the ingredients you see in the image.
+
+Then, using the web search tool, search the web for recipes that can be made with the ingredients they have.
 
 Return recipe suggestions and eventually the recipe instructions to the user, if requested.
 
 """
 
 from langchain.agents import create_agent
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("meta-llama/llama-4-scout-17b-16e-instruct", model_provider="groq")
 
 agent = create_agent(
-    model="gpt-5-nano",
+    model=model,
     tools=[web_search],
-    system_prompt=system_prompt
+    system_prompt=system_prompt,
 )
